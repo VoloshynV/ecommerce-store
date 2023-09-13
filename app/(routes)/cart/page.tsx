b7d5ce4b28b49e79f89bc4da81868cb9afc4c973
Volodymyr Voloshyn <riokin2011@gmail.com>
2023-09-13 13:26:42 +0200

    Create Cart

---
+++ app/(routes)/cart/page.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+import { Container } from "@/components/ui/container";
+import { useCard } from "@/hooks/use-card";
+
+import { CartItem } from "./components/card-item";
+
+const CardPage = () => {
+  const [isMounted, setIsMounted] = useState(false);
+  const { items } = useCard();
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) return null;
+
+  return (
+    <div className="bg-white">
+      <Container>
+        <div className="px-4 py-16 sm:px-6 lg:px-8">
+          <h1 className="text-3xl font-bold text-black">Shopping Cart</h1>
+          <div className="mt-12 gap-x-12 lg:grid lg:grid-cols-12 lg:items-start">
+            <div className="lg:col-span-7">
+              {items.length === 0 && (
+                <p className="text-neutral-500">No items added to cart</p>
+              )}
+              <ul>
+                {items.map((item) => (
+                  <CartItem key={item.id} data={item} />
+                ))}
+              </ul>
+            </div>
+          </div>
+        </div>
+      </Container>
+    </div>
+  );
+};
+
+export default CardPage;
--- components/navbar-actions.tsx
+++ components/navbar-actions.tsx
@@ -23,7 +23,7 @@ export const NavbarActions = () => {
     <div className="ml-auto flex items-center gap-x-4">
       <Button
         className="flex items-center rounded-full bg-black px-4 py-2"
-        onClick={() => router.push("/card")}
+        onClick={() => router.push("/cart")}
       >
         <ShoppingBag size={20} color="white" />
         <span className="ml-2 text-sm font-medium text-white">
