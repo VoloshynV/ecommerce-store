80ab3827b25566310e45fc9a759f21c30eb607f1
Volodymyr Voloshyn <riokin2011@gmail.com>
2023-09-13 11:31:56 +0200

    Create CategoryPage

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -1,23 +1,27 @@
-import { cn } from '@/lib/utils'
-import React, { forwardRef } from 'react'
+import { forwardRef } from "react";
 
-export interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {}
+import { cn } from "@/lib/utils";
+
+export interface ButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement> {}
 
 export const Button = forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ className, children, disabled, type = 'button', ...props }, ref) => {
+  ({ className, children, disabled, type = "button", ...props }, ref) => {
     return (
       <button
         className={cn(
-          'w-auto rounded-full bg-black border-transparent px-5 py-4 disabled:cursor-pointer disabled:opacity-50 text-white font-semibold hover:opacity-75 transition',
-          className
+          "w-auto rounded-full border-transparent bg-black px-5 py-4 font-semibold text-white transition hover:opacity-75 disabled:cursor-pointer disabled:opacity-50",
+          className,
         )}
-        ref={ref}>
+        ref={ref}
+        {...props}
+      >
         {children}
       </button>
-    )
-  }
-)
+    );
+  },
+);
 
-Button.displayName = 'Button'
+Button.displayName = "Button";
 
-export default Button
+export default Button;
