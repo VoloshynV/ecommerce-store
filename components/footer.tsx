7a6642c7db09b312a89642a7087af4b80261d372
Volodymyr Voloshyn <riokin2011@gmail.com>
2023-09-23 14:42:30 +0200

    Add margin

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,11 +1,11 @@
 export const Footer = () => {
   return (
-    <footer className='bg-white border-t'>
-      <div className='mx-auto py-10'>
-        <p className='text-center text-xs text-black'>
+    <footer className="mt-8 border-t bg-white">
+      <div className="mx-auto py-10">
+        <p className="text-center text-xs text-black">
           &copy; 2023 StoreName, Inc. All rights reserved.
         </p>
       </div>
     </footer>
-  )
-}
+  );
+};
